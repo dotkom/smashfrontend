18a8248e11870b03b0064e74e7f4ecc2fe324e7b
haattis <exburn0@gmail.com>
2019-02-28 18:19:20 +0100

    CSS for match modal

--- src/components/Match.js
+++ src/components/Match.js
@@ -53,47 +53,52 @@ class Match extends React.Component {
 
   render(){
     return(
-      <div className="outerComponent">
-        {this.props.winner === this.props.player1._id &&
-          <div className="badgetext left">
-            winner
-          </div>
-        }
-        <div className={"matchComponent "+(this.props.winner===this.props.player1._id ? "left" : "right")}>
-          <div className="matchInformation">
-            <MatchPlayer
-              isWinner={this.props.winner===this.props.player1._id}
-              icon={this.props.character1.id}
-              nick={this.props.player1.nick}
-              character={this.props.character1.name}
-            />
-            <div> VS </div>
-            <MatchPlayer
-              reverse={true}
-              isWinner={this.props.winner===this.props.player2._id}
-              icon={this.props.character2.id}
-              nick={this.props.player2.nick}
-              character={this.props.character2.name}
-            />
-          </div>
-
-          {this.props.isAdmin &&
-            <div className="adminPanel" ref={node => { this.node = node; }}>
-              <button className="deleteButton" onClick={this.modalOpen ? this.closeModal : this.openModal}> Delete </button>
-              {this.state.modalOpen &&
-                <div>
-                  <div> Are you sure? </div>
-                  <button className="deleteButton" onClick={this.delete}> Yes </button>
-                  <button className="deleteButton" onClick={this.closeModal}> No </button>
-
-                </div>
-              }
+      <div className="matchWrapper">
+        <div className="outerComponent">
+          {this.props.winner === this.props.player1._id &&
+            <div className="badgetext left">
+              winner
             </div>
           }
+          <div className={"matchComponent "+(this.props.winner===this.props.player1._id ? "left" : "right")}>
+            <div className="matchInformation">
+              <MatchPlayer
+                isWinner={this.props.winner===this.props.player1._id}
+                icon={this.props.character1.id}
+                nick={this.props.player1.nick}
+                character={this.props.character1.name}
+              />
+              <div> VS </div>
+              <MatchPlayer
+                reverse={true}
+                isWinner={this.props.winner===this.props.player2._id}
+                icon={this.props.character2.id}
+                nick={this.props.player2.nick}
+                character={this.props.character2.name}
+              />
+            </div>
+
+
+          </div>
+          {this.props.winner === this.props.player2._id &&
+            <div className="badgetext right">
+              winner
+            </div>
+          }
+
         </div>
-        {this.props.winner === this.props.player2._id &&
-          <div className="badgetext right">
-            winner
+        {this.props.isAdmin &&
+          <div className="adminPanel" ref={node => { this.node = node; }}>
+            <button className="deleteButton" onClick={this.modalOpen ? this.closeModal : this.openModal}> Delete </button>
+            {this.state.modalOpen &&
+              <div className="matchModal">
+                <div> Are you sure? </div>
+                <div className="buttons">
+                  <button className="deleteButton" onClick={this.delete}> Yes </button>
+                  <button className="deleteButton" onClick={this.closeModal}> No </button>
+                </div>
+              </div>
+            }
           </div>
         }
       </div>
