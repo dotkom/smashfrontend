37f0c999c2f5662d0023c90a071cc08b6a2e915b
Henrik Liodden <exburn0@gmail.com>
2019-03-09 11:03:31 +0100

    container linting

---
+++ src/containers/Leaderboard.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import '../styles/leaderboard.css';
+import PropTypes from 'prop-types';
+import { getLeaderboard, pageReset } from '../actions/leaderboard';
+import User from '../components/User';
+
+
+class Leaderboard extends React.Component {
+  static propTypes = {
+    pageReset: PropTypes.func.isRequired,
+    getLeaderboard: PropTypes.func.isRequired,
+    users: PropTypes.array.isRequired,
+    allLoaded: PropTypes.bool.isRequired,
+    page: PropTypes.number.isRequired,
+  }
+
+  async componentDidMount() {
+    await this.props.pageReset();
+    const { page } = this.props;
+    this.props.getLeaderboard(page);
+  }
+
+
+  render() {
+    return (
+      <div className="leaderboardContainer">
+        <div className="labels">
+          <div>#</div>
+          <div>rating</div>
+          <div>nick</div>
+        </div>
+        <div className="userContainer">
+          { this.props.users.map((item, index) => (
+            <User
+              key={item._id}
+              number={index + 1}
+              nick={item.nick}
+              id={item._id}
+              rating={item.rating}
+            />
+          ))
+        }
+        </div>
+        {!this.props.allLoaded && this.props.users.length > 0
+        && <button type="button" onClick={() => this.props.getLeaderboard(this.props.page)}> load more </button>
+      }
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isLoading: state.leaderboard.isLoading,
+  page: state.leaderboard.page,
+  users: state.leaderboard.users,
+  allLoaded: state.leaderboard.allLoaded,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getLeaderboard: page => dispatch(getLeaderboard(page)),
+  pageReset: () => dispatch(pageReset()),
+
+
+});
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Leaderboard);
---
+++ src/containers/Matches.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import '../styles/matches.css';
+import PropTypes from 'prop-types';
+import { getAllMatches, deleteMatch, pageReset } from '../actions/matches';
+import Match from '../components/Match';
+
+
+class Matches extends React.Component {
+  static propTypes = {
+    pageReset: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    getMatches: PropTypes.func.isRequired,
+    matches: PropTypes.array.isRequired,
+    deleteMatch: PropTypes.func.isRequired,
+    user: PropTypes.object.isRequired,
+    isAdmin: PropTypes.bool.isRequired,
+    allLoaded: PropTypes.bool.isRequired,
+  }
+
+  async componentDidMount() {
+    await this.props.pageReset();
+    this.props.getMatches(this.props.page);
+  }
+
+  render() {
+    return (
+      <div className="matchesPage">
+        <div className="matchContainer">
+          { this.props.matches.map(item => (
+            <Match
+              key={item._id}
+              id={item._id}
+              player1={item.player1}
+              oldrank1={item.oldrank1}
+              oldrank2={item.oldrank2}
+              newrank1={item.newrank1}
+              newrank2={item.newrank2}
+              player2={item.player2}
+              character1={item.character1}
+              character2={item.character2}
+              winner={item.winner}
+              deleteMatch={this.props.deleteMatch}
+              date={item.date}
+              showAdmin={this.props.user && this.props.user.isAdmin && false}
+            />
+          ))
+          }
+        </div>
+        { !this.props.allLoaded
+          && <button type="button" onClick={() => this.props.getMatches(this.props.page)}> Get matches </button>
+        }
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isLoading: state.matches.isLoading,
+  user: state.auth.user,
+  matches: state.matches.matches,
+  page: state.matches.page,
+  allLoaded: state.matches.allLoaded,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getMatches: page => dispatch(getAllMatches(page)),
+  deleteMatch: id => dispatch(deleteMatch(id)),
+  pageReset: () => dispatch(pageReset()),
+
+
+});
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Matches);
