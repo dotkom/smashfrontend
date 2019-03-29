7712b80008cd2172b92d2e2981c57f55be053430
haattis <exburn0@gmail.com>
2019-03-29 21:05:58 +0100

    Nicknames on leaderboard now links to their profile

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import '../styles/user.css';
 
 
 const User = ({
-  number, rating, nick,
+  number, rating, nick, id,
 }) => (
   <div className="user">
     <div className="number">
-      {' '}
       {number}
-      {' '}
     </div>
     <div className="rating">
-      {' '}
       {Math.round(rating)}
-      {' '}
     </div>
     <div className="nick">
-      {' '}
-      {nick}
-      {' '}
+      <Link to={`/profile/${id}`}>{nick}</Link>
     </div>
     <div />
 
@@ -31,6 +26,7 @@ User.propTypes = {
   number: PropTypes.number.isRequired,
   rating: PropTypes.number.isRequired,
   nick: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 
